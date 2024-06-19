da53425018b8e18bf07f30be8033f314c454932b
KMS06 <jjm336949@dsm.hs.kr>
2024-06-19 13:47:31 +0900

    init: WebConfig
    cors

---
+++ src/main/java/com/example/muckja/global/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.muckja.global.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
